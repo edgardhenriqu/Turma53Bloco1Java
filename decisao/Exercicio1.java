d384b8bd57b0a578d70ed0626ec255b9ee18860f
Edgard Henrique <edigarhenriqu@gmail.com>
2022-05-09 18:30:19 -0300

    JAVA

---
+++ decisao/Exercicio1.java
@@ -0,0 +1,26 @@
+package decisao;
+
+import java.util.Scanner;
+
+public class Exercicio1 
+{
+
+
+	public static void main(String[] args) 
+	{
+	
+			int[] vetor = new int[4];
+			int i, maior = 0;
+			try (Scanner input = new Scanner(System.in)) {
+				for(i = 0; i < 3; i++)
+				{
+						System.out.print("Digite um valor: ");
+						vetor[i] = input.nextInt();
+						if(vetor[i] > maior)
+							maior = vetor[i];
+				}
+			}
+			
+			System.out.println("O maior valor é: "+maior);
+	}
+}
