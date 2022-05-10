3d1765d9c93c544ea3280e8ec782feb23a1f309c
Edgard Henrique <edigarhenriqu@gmail.com>
2022-05-10 18:00:11 -0300

    laços

---
+++ repeticaoFOR/exercicio1FOR.java
@@ -0,0 +1,12 @@
+package repeticaoFOR;
+
+public class exercicio1FOR {
+	public static void main(String[] args) {
+			for (int cont = 1000; cont < 2000; cont++) {
+					if (cont % 11 == 5) {
+							System.out.println(cont);
+					}
+			}
+	}
+
+}
