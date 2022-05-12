102036dddc1e0a1b49f73dbfb46facd52b907e77
Edgard Henrique <edigarhenriqu@gmail.com>
2022-05-11 22:27:57 -0300

    Orientação ao Objeto

---
+++ OrientacaoAoObjeto01/Cliente.java
@@ -0,0 +1,47 @@
+package OrientacaoAoObjeto01;
+
+import java.util.Date;
+
+public class Cliente {
+	
+	private String nome, email;
+	private Date dtNascimento;
+	private Integer cpf;
+	
+	public String getNome( ) {
+			return nome;
+	}
+	
+	public void setNome(String nome) {
+			this.nome = nome;
+	}
+	
+	public String getEmail( ) {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+			this.email = email;
+	}
+	
+	public Date getDtNascimento( ) {
+			return dtNascimento;
+	}
+	
+	public void getDtNascimento(Date date) {
+			this.dtNascimento = date;
+	}
+	
+	public Integer getCpf( ) {
+			return cpf;
+	}
+	
+	public void setCpf(Integer cpf) {
+			this.cpf = cpf;
+	}
+	
+	void comprar() {
+			System.out.println("\n\nO Cliente está escolhendo o produto");
+	}
+
+}
